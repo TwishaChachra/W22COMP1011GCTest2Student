5ee239badbf09a36917a340f9ccd77471e8ec69b
Twisha Chachra <twishachachra@gmail.com>
2022-08-18 09:15:39 -0400

    Question 1 and 2

--- W22COMP1011GCTest2Student/src/main/java/com/example/w22comp1011gctest2student/Customer.java
+++ W22COMP1011GCTest2Student/src/main/java/com/example/w22comp1011gctest2student/Customer.java
@@ -1,5 +1,58 @@
 package com.example.w22comp1011gctest2student;
 
 public class Customer {
+    public Customer(int customerId, String firstName, String lastName, String phone, ArrayList<Product> products) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.products = products;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private ArrayList<Product> products;
 
 }
--- W22COMP1011GCTest2Student/src/main/java/com/example/w22comp1011gctest2student/Product.java
+++ W22COMP1011GCTest2Student/src/main/java/com/example/w22comp1011gctest2student/Product.java
@@ -1,4 +1,44 @@
 package com.example.w22comp1011gctest2student;
 
 public class Product {
+  private int sku;
+  private String name;
+
+  public Product(int sku, String name, double salePrice, double regularPrice, String urlImage) {
+    this.sku = sku;
+    this.name = name;
+    this.salePrice = salePrice;
+    this.regularPrice = regularPrice;
+    this.urlImage = urlImage;
   }
+
+  public void setSku(int sku) {
+    this.sku = sku;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setSalePrice(double salePrice) {
+    this.salePrice = salePrice;
+  }
+
+  public void setRegularPrice(double regularPrice) {
+    this.regularPrice = regularPrice;
+  }
+
+  public void setUrlImage(String urlImage) {
+    this.urlImage = urlImage;
+  }
+
+  private double salePrice;
+  private double regularPrice;
+  private String urlImage;
+
+  @java.lang.Override
+  public java.lang.String toString() {
+    String formattedSalesPrice = String.format("%.2f", salePrice)
+    return name + "-$" + formattedSalesPrice;
+  }
+}
